1da0c0ab73c559e438d35f018cadfc89ee9d16cf
gjc9620 <gujiacheng@hys-inc.cn>
2019-03-16 20:32:14 +0800

    learning

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<h1>My Todo App!</h1>
+		<TodoList/>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from './components/TodoList.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+	components: {
+		TodoList
+	}
 }
 </script>
 
-<style>
+<style lang="scss">
+@import './variables.scss';
+
+*, *::before, *::after {
+	box-sizing: border-box;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	max-width: 400px;
+	margin: 0 auto;
+	line-height: 1.4;
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	color: $vue-blue;
+}
+
+h1 {
+	text-align: center;
 }
 </style>
---
+++ src/components/TodoListItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <li>
+    {{ todo.text }} <button @click="$emit('remove', todo.id);">X</button>
+    <div v-html="1">aaba</div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    todo: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
